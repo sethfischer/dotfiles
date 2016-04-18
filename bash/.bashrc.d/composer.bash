4b01888bc620bd438a7498badf8cf6e24fc81c8f
Seth Fischer <seth@fischer.nz>
2016-04-18 16:19:31 +1200

    Correct comment

--- bash/.bashrc.d/composer.bash
+++ bash/.bashrc.d/composer.bash
@@ -1,4 +1,4 @@
-# # Make commands installed with 'composer global require' available.
+# Make commands installed with 'composer global require' available.
 if [ -d "$HOME/.config/composer/vendor/bin" ] ; then
     PATH="$PATH:$HOME/.config/composer/vendor/bin"
 fi
